e8055b8a8330996e0bf989e4b06d93b84b0ac744
moreee-sa <morenolms41@gmail.com>
2025-10-31 15:55:38 +0100

    Aggiunto supporto al percorso personalizzato nel componente NavLogo

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <div className="bg-[#24252a] w-full flex items-center px-20 py-6 sticky top-0 z-10">
       <div className="w-[40%] h-full flex items-center">
-        <NavLogo brand="ESSE" />
+        <NavLogo brand="ESSE" path="/" />
       </div>
       <div className="w-full h-full flex justify-end items-center gap-4">
         <NavLink name="Home" path="/" />
