dee597ff8803ea971610fe43704bf1aac3ba3599
moreee-sa <morenolms41@gmail.com>
2025-10-31 19:35:33 +0100

    Corretto layout non responsivo della sezione introduttiva su mobile

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,20 +4,20 @@ function Home() {
   return (
     <>
       <Navbar />
-      <div className="px-20 py-6">
+      <div className="p-6 lg:px-20 lg:py-6">
         {/* Introduction */}
-        <div className="flex flex-col justify-center items-center lg:flex-row rounded-2xl overflow-hidden w-full">
+        <div className="flex flex-col justify-center items-center lg:flex-row rounded-xl overflow-hidden w-full">
           {/* Text */}
           <div className="w-full lg:w-[40%] flex flex-col p-4">
             <span
               style={{ fontFamily: "'Libre Baskerville', serif", fontWeight: 400 }}
-              className="text-stone-900 text-8xl md:text-6xl text-wrap"
+              className="text-stone-900 text-3xl lg:text-8xl md:text-6xl text-wrap"
             >
               Dal 2025
             </span>
             <span
               style={{ fontFamily: "'Libre Baskerville', serif", fontWeight: 400 }}
-              className="text-stone-700 text-7xl md:text-5xl text-wrap"
+              className="text-stone-700 text-2xl lg:text-7xl md:text-5xl leading-7 md:leading-18 text-wrap"
             >
               creatività servita con la cura di un buon caffè
             </span>
@@ -28,7 +28,7 @@ function Home() {
             <img
               src="image/drink_1.webp"
               alt=""
-              className="object-cover w-full h-full rounded-2xl lg:rounded-none"
+              className="object-cover w-full h-full rounded-xl lg:rounded-none"
             />
             <div
               className="absolute inset-0 bg-none lg:bg-linear-[90deg_in_oklab,rgba(255,255,255,1)_10%,rgba(208,212,203,0.2)_25%,rgba(0,0,0,0)_100%]"
