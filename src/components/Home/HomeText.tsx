de04094b7e87dc8b27bb342d1e460692bf5276b5
moreee-sa <morenolms41@gmail.com>
2025-11-01 16:47:17 +0100

    Aggiornato HomeText con nuovo layout, font e pulsante collegato al tema

--- src/components/Home/HomeText.tsx
+++ src/components/Home/HomeText.tsx
@@ -1,31 +1,50 @@
 import FadeContent from "@components/FadeContent"
+import { IoRestaurantOutline } from "react-icons/io5";
+import { useTheme } from "@components/Theme/ThemeContext";
 
 function HomeText() {
+  const { primary, text } = useTheme();
+  
   return (
-    <div className="w-full lg:w-[40%] flex flex-col p-4">
-      <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
+    <div className="flex flex-col justify-center md:justify-start absolute inset-0 z-10 w-full p-6 md:p-20 lg:px-52 lg:py-24 md:w-[70vw] lg:w-[80vw]">
+      <FadeContent blur={true} duration={800} delay={50} easing="ease-out" initialOpacity={0}>
         <span
-          style={{ fontFamily: "'Libre Baskerville', serif", fontWeight: 400 }}
-          className="text-stone-900 text-3xl lg:text-8xl md:text-6xl text-wrap"
+          style={{
+            fontFamily: "'Mochiy Pop One', sans-serif",
+          }}
+          className="text-[#faf5f3] text-3xl md:text-5xl lg:text-7xl text-wrap"
         >
-          Dal 2025
+          ESSE
         </span>
       </FadeContent>
-      <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
+
+      <FadeContent blur={true} duration={800} delay={100} easing="ease-out" initialOpacity={0} className="md:mt-4 lg:mt-7">
         <span
-          style={{ fontFamily: "'Libre Baskerville', serif", fontWeight: 400 }}
-          className="text-stone-700 text-2xl lg:text-7xl md:text-5xl leading-7 md:leading-18 text-wrap"
-          >
-          creatività servita con la cura di un buon caffè
+          style={{
+            fontFamily: "'Nunito Sans Variable', sans-serif",
+            fontWeight: 200,
+          }}
+          className="text-[#faf5f3] text-2xl md:text-4xl lg:text-6xl leading-8 lg:leading-16 text-wrap"
+        >
+          Assapora l’arte di un caffè perfetto!
         </span>
       </FadeContent>
-      <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
-        <span
-          style={{ fontFamily: "'Libre Baskerville', serif", fontWeight: 400 }}
-          className="text-stone-700 text-base lg:text-lg leading-5 md:leading-6 text-wrap mt-6"
+
+      <FadeContent blur={true} duration={800} delay={200} easing="ease-out" initialOpacity={0}>
+        <div className="mt-3 md:mt-9">
+          <button
+            className="flex gap-1.5 items-center justify-center hover:bg-[#db812d] transition-colors duration-200 rounded-full p-3 md:p-5 text-sm md:text-lg "
+            style={{ 
+              fontFamily: "'Nunito Sans Variable', sans-serif",
+              fontWeight: 300,
+              backgroundColor: primary,
+              color: text
+            }}
           >
-          La nostra storia inizia oggi, con la stessa passione di chi ogni giorno crea esperienze da gustare, una tazza alla volta.
-        </span>
+            <IoRestaurantOutline color={text} size={18} />
+            Apri Menu
+          </button>
+        </div>
       </FadeContent>
     </div>
   )
