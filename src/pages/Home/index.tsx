650f283d0d4b4f8a498e4023a28adeea582952ec
moreee-sa <morenolms41@gmail.com>
2025-11-01 17:36:57 +0100

    Aggiunta SpecialtiesSection alla pagina Home sotto HomeSection

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@components/Navbar"
 import HomeSection from "@components/Home/HomeSection"
+import SpecialtiesSection from "@components/Home/SpecialtiesSection"
 
 function Home() {
   return (
     <>
       <Navbar />
       <HomeSection />
+      <SpecialtiesSection />
     </>
   )
 }
