6908afd24f81b20fd32345b5905e9eb8b2e421c4
Moreno Sinagra <76905821+moreee-sa@users.noreply.github.com>
2025-11-01 16:59:08 +0100

    Feature/home content (#8)
    * Aggiunto --host allo script dev per permettere accesso locale da altri dispositivi
    
    * Rimossa vecchia immagine e aggiunta nuova
    
    * Sostituita immagine e aggiunto overlay scuro per maggiore contrasto
    
    * Aggiornato HomeText con nuovo layout, font e pulsante collegato al tema
    
    * Aggiornata HomeSection: layout verticale, HomeImage come sfondo e HomeText sopra
    
    * Modificata altezza div principale: 60vh su mobile/medium, 80vh su desktop
    
    * Cambiata proprietà justify-start su lg invece che md in HomeText
    
    * Ridotte dimensioni del testo in HomeText per migliorare leggibilità su desktop e mobile

--- src/components/Home/HomeSection.tsx
+++ src/components/Home/HomeSection.tsx
@@ -3,11 +3,9 @@ import HomeImage from "@components/Home/HomeImage"
 
 function HomeSection() {
   return (
-    <div className="p-6 lg:px-20 lg:py-6">
-        <div className="flex flex-col justify-center items-center lg:flex-row rounded-xl overflow-hidden w-full">
-          <HomeText />
-          <HomeImage />
-        </div>
+    <div className="w-full h-[60vh] md:h-[60vh] lg:h-[80vh] relative overflow-hidden">
+      <HomeImage />
+      <HomeText />
     </div>
   )
 }
