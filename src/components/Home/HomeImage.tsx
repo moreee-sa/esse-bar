9c145cc438223dd4bbf0467129ca2d5dd4a8e013
moreee-sa <morenolms41@gmail.com>
2025-11-01 16:46:54 +0100

    Sostituita immagine e aggiunto overlay scuro per maggiore contrasto

--- src/components/Home/HomeImage.tsx
+++ src/components/Home/HomeImage.tsx
@@ -1,20 +1,14 @@
-import FadeContent from "@components/FadeContent"
-
 function HomeImage() {
   return (
-    <div className="relative w-full lg:w-[60%]">
-      <FadeContent blur={true} duration={1500} easing="ease-out" initialOpacity={0}>
-          <img
-            src="image/drink_1.webp"
-            alt=""
-            className="object-cover w-full h-full rounded-md lg:rounded-none"
-            />
-          <div
-            className="absolute inset-0 bg-none lg:bg-linear-[90deg_in_oklab,rgba(255,255,255,1)_10%,rgba(208,212,203,0.2)_25%,rgba(0,0,0,0)_100%]"
-            />
-      </FadeContent>
+    <div className="w-full h-full">
+      <div className="absolute inset-0 bg-black/40" />
+      <img
+        src="image/coffee_1.webp"
+        alt=""
+        className="w-full h-full object-cover object-top"
+      />
     </div>
-  )
+  );
 }
 
 export default HomeImage
