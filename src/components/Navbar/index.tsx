f01536d24b3a98b4c8f0a700a2a3a03c36588675
moreee-sa <morenolms41@gmail.com>
2025-11-29 15:49:58 +0100

    Rimosso ThemeContext.Provider dalla navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,12 @@
 import NavbarDesktop from "@components/Navbar/NavbarDesktop"
 import NavbarMobile from "@components/Navbar/NavbarMobile"
-import ThemeContext, { defaultTheme } from "@components/Theme/ThemeContext"
 
 function Navbar() {
   return (
-    <ThemeContext.Provider value={defaultTheme}>
+    <>
       <NavbarDesktop />
       <NavbarMobile />
-    </ThemeContext.Provider>
+    </>
   )
 }
 
