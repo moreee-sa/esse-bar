3c92a7af9cfa4757a4cee766a0b5e3af2dbb504b
moreee-sa <morenolms41@gmail.com>
2025-11-29 15:57:21 +0100

    Aggiornata NavbarMobile per usare THEME invece di ThemeContext

--- src/components/Navbar/NavbarMobile.tsx
+++ src/components/Navbar/NavbarMobile.tsx
@@ -2,11 +2,9 @@ import { useState } from "react"
 import NavLogo from "@components/Navbar/NavLogo"
 import NavLink from "@components/Navbar/NavLink";
 import { IoCloseOutline, IoMenuOutline  } from "react-icons/io5";
-import { useTheme } from "../Theme/ThemeContext";
+import { THEME } from "@/lib/constants";
 
 function NavbarMobile() {
-  const { primary, secondary, text } = useTheme();
-
   const [isOpen, setIsOpen] = useState(false);
 
   function handleMenu() {
@@ -18,24 +16,24 @@ function NavbarMobile() {
     <>
       <div
         className="w-full items-center p-6 sticky top-0 z-50 flex md:hidden"
-        style={{ backgroundColor: primary }}
+        style={{ backgroundColor: THEME.navbar }}
       >
         <div className="w-full h-full flex items-center">
           <NavLogo brand="ESSE" path="/" />
         </div>
         <div className="w-full h-full flex justify-end items-center" onClick={() => handleMenu()}>
-          <IoMenuOutline color={text} size={35} />
+          <IoMenuOutline color={THEME.text} size={35} />
         </div>
       </div>
 
-      <div className={`z-50 fixed top-0 right-0 w-full h-full transition-[backdrop-filter] duration-300 md:hidden ${isOpen ? 'translate-x-0 backdrop-blur-xs' : 'translate-x-full'}`} onClick={() => handleMenu()} />
+      <div className={`z-50 fixed top-0 right-0 w-full h-full transition-[backdrop-filter] duration-600 md:hidden ${isOpen ? 'translate-x-0 backdrop-blur-xs' : 'translate-x-full'}`} onClick={() => handleMenu()} />
 
       <div
         className={`z-60 fixed top-0 right-0 w-[250px] h-full transform transition-transform duration-300 md:hidden ${isOpen ? 'translate-x-0' : 'translate-x-full'}`}
-        style={{ backgroundColor: secondary }}
+        style={{ backgroundColor: THEME.navbar }}
       >
         <div className="h-20 flex flex-col items-end justify-center p-6" onClick={() => handleMenu()}>
-          {isOpen && <IoCloseOutline color={text} size={35} />}
+          {isOpen && <IoCloseOutline color={THEME.text} size={35} />}
         </div>
         <div className="flex flex-col items-center gap-4">
           <NavLink name="Home" path="/" />
