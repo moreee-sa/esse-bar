39436e45d314919524f0166223d919cd49de42e7
moreee-sa <morenolms41@gmail.com>
2025-11-29 15:48:00 +0100

    Dichiarato il modulo @fontsource-variable/playfair-display in fonts.d.ts

--- src/types/fonts.d.ts
+++ src/types/fonts.d.ts
@@ -1,3 +1,4 @@
 declare module '@fontsource-variable/nunito-sans';
 declare module '@fontsource/mochiy-pop-one';
 declare module '@fontsource-variable/manrope';
+declare module '@fontsource-variable/playfair-display';
