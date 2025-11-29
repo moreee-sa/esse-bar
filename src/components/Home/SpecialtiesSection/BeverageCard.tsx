0279c5dd49e862139df56a4b1de0b77db7767e0c
moreee-sa <morenolms41@gmail.com>
2025-11-29 17:10:58 +0100

    Aggiornata BeverageCard per usare THEME e font Playfair Display

--- src/components/Home/SpecialtiesSection/BeverageCard.tsx
+++ src/components/Home/SpecialtiesSection/BeverageCard.tsx
@@ -1,24 +1,24 @@
-import { useTheme } from "@/components/Theme/ThemeContext";
 import { FaCoffee } from "react-icons/fa"
+import { THEME } from "@/lib/constants";
+import '@fontsource-variable/playfair-display';
 
 type BeverageCardProps = {
   title: string;
   description: string;
 }
 
-function BeverageCard({ title, description }: BeverageCardProps) {
-  const { primary, text } = useTheme();
-  
+function BeverageCard({ title, description }: BeverageCardProps) {  
   return (
     <div className="flex flex-col items-center justify-start w-40 h-full">
       <div className="bg-[#904a1a] rounded-full p-4">
-        <FaCoffee color={primary} size={50} />
+        <FaCoffee color={THEME.text} size={50} />
       </div>
       <span
         className="text-2xl md:text-3xl"
         style={{
-          fontFamily: "'Mochiy Pop One', sans-serif",
-          color: text,
+          color: THEME.text,
+          fontFamily: "'Playfair Display Variable', serif",
+          fontWeight: 400,
         }}
       >
         {title}
@@ -26,9 +26,9 @@ function BeverageCard({ title, description }: BeverageCardProps) {
       <span
         className="text-lg md:text-xl mt-2 text-center p-1"
         style={{
+          color: THEME.text,
           fontFamily: "'Nunito Sans Variable', sans-serif",
           fontWeight: 300,
-          color: text,
         }}
       >
         {description}
